92de5d80f5d8d009b2795b940f868845a58f2377
Maryam Kaka <mkaka139@gmail.com>
2018-10-13 17:46:34 -0400

    changed it to a get (cus why not)

--- app.js
+++ app.js
@@ -9,53 +9,44 @@ const state = {
 	AgentState: "NOT_READY",
 	Number: "+14699123081"
 }
-app.get('/', function (req, res)
-{
+app.get('/', function (req, res) {
 	res.send(JSON.stringify({ Hello: 'World' }));
 });
 
-app.get('/get-state', function (req, res)
-{
+app.get('/get-state', function (req, res) {
 	res.send(state);
 });
 
-app.post('/set-state', function (req, res)
-{
+app.get('/set-state', function (req, res) {
 	const _phoneState = req.body.PhoneState;
 	const _agentState = req.body.AgentState;
 	const _number = req.body.Number;
 
-	if (_number === "Easter Egg")
-	{
+	if (_number === "Easter Egg") {
 		res.send(state);
 		return;
 	}
 
-	if (_phoneState === "Call" || _phoneState === "Hold" || _phoneState === "Idle")
-	{
+	if (_phoneState === "Call" || _phoneState === "Hold" || _phoneState === "Idle") {
 		state.PhoneState = _phoneState;
 	}
 
-	if (IsValidAgentState(_agentState))
-	{
+	if (IsValidAgentState(_agentState)) {
 		state.AgentState = _agentState;
 	}
 
-	if (_number.length === 11)
-	{
+	if (_number.length === 11) {
 		state.Number = _number;
 	}
 
 	res.send(state);
 });
 
-app.listen(port, function ()
-{
+app.listen(port, function () {
 	console.log(`Example app listening on port !`);
 });
 
-const IsValidAgentState = function (agentState)
-{
+const IsValidAgentState = function (agentState) {
 	return agentState === "READY"
 		|| agentState === "NOT_READY"
 		|| agentState === "NOT_READY_AFTER_CALLWORK"

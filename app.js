5eade11c211510be0e59def754060322d21cf286
INGENIUS\justin.fleming <justin.fleming@ingenius.com>
2018-10-13 16:24:23 -0400

    getstate

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
+const state = {
+	PhoneState: "Idle",
+	AgentState: "NotReady",
+}
 app.get('/', function (req, res)
 {
 	res.send(JSON.stringify({ Hello: 'World' }));
 });
+
+app.get('/getState', function (req, res)
+{
+	res.send(state);
+});
+
 app.listen(port, function ()
 {
 	console.log(`Example app listening on port !`);
